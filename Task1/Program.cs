7c3c7080e0e55d5c2cd5e81c26526e2057a8f237
Daria <gdaria1609@yandex.ru>
2023-09-13 19:59:18 +0500

    add_Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью
+
+
+string Nambers(int n)
+{
+    if (n >= 1) return $"{n}" + "\t" + Nambers(n - 1);
+    else
+    {
+        Console.Write("Вы ввели не натуральное число. Программа завершит свою работу. Попробуйте снова.");
+        Console.ReadLine();
+        return String.Empty;
+    }
+}
+
+
+Console.WriteLine("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(Nambers(n));
